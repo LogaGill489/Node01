18a3cde762dbe710c05747f2940f4d674475c2ae
LogaGill489 <logagill489@ed.amdsb.ca>
2025-01-09 21:32:08 -0500

    Finished implementing require syntax.

---
+++ functions.js
@@ -0,0 +1,11 @@
+//how to have multiple functions
+//with a single object
+
+module.exports = { //object
+ f1: () => { //function #1
+    console.log('Hello');
+ },
+ f2: () => { //function #2
+    console.log('World');
+ },
+}
--- index2.js
+++ index2.js
@@ -14,3 +14,6 @@ require('./index.js')(); //returns a new instance of the function
                          //declared in module.exports ... each time
 require('./index.js')();
 require('./index.js')();
+
+require('./functions.js').f1(); //runs the desired function as declared
+require('./functions.js').f2(); //in the exports object
