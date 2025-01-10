e5efb76981161d20bf97d46ce82edd34b60f65fd
LogaGill489 <logagill489@ed.amdsb.ca>
2025-01-09 20:27:47 -0500

    First test of some JavaScript functions

---
+++ index.js
@@ -0,0 +1,17 @@
+//Write a random math value
+/*
+console.log(Math.random()); //outputs on the console
+console.log(process); //outputs the processes
+console.dir(process, { depth: 0 }); //outputs the first value in each process (i.e. no nesting)
+*/
+
+// (function(exports, require, module, __filename, __dirname) {
+console.log(arguments);
+//return ...
+// })()
+
+function X() {
+    console.log(arguments); //returns arguements passed to X
+}
+
+X(1, 7, 9);
