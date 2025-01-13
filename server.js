152cd0a844fa29d2112f5f20e2f68857025389dd
LogaGill489 <logagill489@ed.amdsb.ca>
2025-01-12 19:56:31 -0500

    Created my first server! + added a few extra lines to commands.txt and added an example to package.json.

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 console.log('Server Boot.');
 
 // Native JavaScript Web Server
+
+import http from 'http';
+
+const server = http.createServer();
+
+server.listen(3000); //starts listening for events in the server on port 3000
+
+//events
+
+//req is the request, res it the response
+//every time you change something you need to restart node (i.e add a new event to the website)
+server.on('request', (req, res) => {
+    console.log('Request!!!', req.url);
+    res.write('Hello HTTP');
+    res.end();
+});
