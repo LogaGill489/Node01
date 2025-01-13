62e4015e66c486e83a3f8f43ef477b0dd2b6539d
LogaGill489 <logagill489@ed.amdsb.ca>
2025-01-13 13:22:45 -0500

    Added the nodemon library, helping with updating servers while they are running.

--- expressServer.js
+++ expressServer.js
@@ -4,11 +4,14 @@ const server = express();
 server.listen(3000);
 
 server.get('/', (req, res) => {
-    res.send({message: 'Hello Express'});
+    res.send({message: 'Hello Non Favicon'});
+    console.log(req.url);
 });
 
 //* replaces .ico, working as a generic domain
 server.get('/favicon.*', (req, res) => {
     res.send({message: 'Hello Express'});
     res.sendFile('./favicon....');
+    console.log(req.url);
 });
+
