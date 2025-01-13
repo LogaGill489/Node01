6c65ab0f713174d923252050ea48293fd6c064e9
LogaGill489 <logagill489@ed.amdsb.ca>
2025-01-12 19:30:04 -0500

    Created a commands.txt file, and moved some of the comments as to prevent clutter.

--- port1.js
+++ port1.js
@@ -1,15 +1,3 @@
-//shell command that allows access in the event that
-//running scripts are disabled:
-//Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
-
-//when installing packages, using the URL from a github will tell npm that you
-//want that package directly from the Github repo, example:
-//nongit -> npm i express
-//github -> npm i expressjs/express
-//https://github.com/expressjs/express
-
-//you can also remove a package by using the command -> npm uninstall <package>
-
 export default { 
     f1: () => { //function #1
         console.log('Hello');
--- server.js
+++ server.js
@@ -1,8 +1,3 @@
 console.log('Server Boot.');
 
-//important:
-//in package.json, under the script section
-//writing "start": "run code", i.e.: "start": "node .\\server.js"
-//we can run this by simply running the command npm start
-
 // Native JavaScript Web Server
