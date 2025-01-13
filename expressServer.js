74a5ffde00b73b48f95d6a59939b3e7733038a98
LogaGill489 <logagill489@ed.amdsb.ca>
2025-01-13 16:16:55 -0500

    Added some basic variable math in port1.js. Added executables from the child_process library.

--- expressServer.js
+++ expressServer.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { exec } from 'child_process';
 
 const server = express();
 server.listen(3000);
@@ -15,3 +16,23 @@ server.get('/favicon.*', (req, res) => {
     console.log(req.url);
 });
 
+//callback method
+exec('ls /', (err, stdout, stderr) => {
+    if (err) {
+        process.exit(1);
+    }
+    console.log(stdout);
+});
+
+//cleaner method (promise method)
+const main = async () => {
+    try { //replaces the actual error in the callback method
+        const { stdout, stderr } = await exec('ls /');
+        console.log(stdout);
+    }
+    catch (error) {
+        process.exit(1);
+    }
+}
+
+main();
--- port1.js
+++ port1.js
@@ -6,3 +6,18 @@ export default {
         console.log('World');
      },
 }
+
+//basic math using javascript notation
+//unlike c-based langs, the variables are automatically assumed to be integers
+function main () {
+ var x = 73;
+ var y = 33;
+ console.log(x + y);
+}
+
+main();
+
+//coding levels still apply
+var x = 2;
+var y = 3;
+console.log(x + y);
